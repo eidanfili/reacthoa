728fe7725bfa53f1986b602635847c8720026c6b
eidanfili <eidan@ledezma.org>
2018-10-22 15:27:52 -0600

    form title component

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { FormTitle } from '../formTitle';
 
 class Signin extends Component {
     render() {
         return(
-            <div className="sign-in">sign in</div>
+            <div className="sign-in">
+                <FormTitle className="sign-in__title" text="login"/>
+            </div>
         )
     }
 }
