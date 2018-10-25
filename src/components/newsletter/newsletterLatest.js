0f184817ace2af607130f5ac8f87ebc2aee124d4
eidanfili <eidan@ledezma.org>
2018-10-25 11:31:34 -0600

    added a buttoin component

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
+    
+
+function Button({callback, text, icon, className}) {
+    if(icon) {
+        return (
+            <a onClick={callback} className={`${className} button`}>
+                <i className={icon}></i>
+            </a>
+        )
+    }
+    
+}
+
 class NewsletterLatest extends Component {
+
+    handleEdit = () => {
+        console.log('yeet');
+    }
+    
     render() {
         const { title, body, imageURL } = this.props;
         return(
             <div className="newsletter-latest">
                 <h2 className="newsletter-latest__title">{title}</h2>
                 <img className="newsletter-latest__image" src={imageURL}/>
+                <Button className="newsletter-latest__button" callback={() => this.handleEdit()} icon="fas fa-pen"/>
                 <div className="newsletter-latest__body">
                     <p>{body}</p>
                 </div>
